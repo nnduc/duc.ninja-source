cf1a1495bde65f02a3a0b6c5ac6a1b39ca441dd3
Duc Nguyen <duc.nguyenngoc@axonactive.com>
2019-05-25 08:44:35 +0700

    Inclusive enums with OptionSet

--- duc-deploy-github.sh
+++ duc-deploy-github.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -v
-rm -frd .deploy_git && git clone git@github.com:nnduc/nnduc.github.io.git --branch master .deploy_git --depth 1
+
+rm -frd .deploy_git
+
+git clone git@github.com:nnduc/nnduc.github.io.git --branch publishing .deploy_git --depth 1
+
 rm -frd .deploy_git/*
+
 hexo generator && hexo deploy
