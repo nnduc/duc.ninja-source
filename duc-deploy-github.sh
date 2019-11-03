ea6efa29b08936c892c53db36748f2ca2dc8e9d4
Duc <ducito@msn.com>
2019-11-03 21:36:47 +0700

    Add nov-2-swift-5-1-Using-self-to-refer-to-enclosing-types.md

--- duc-deploy-github.sh
+++ duc-deploy-github.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ve
 
-rm -frd .deploy_git || exit 1
+rm -frd .deploy_git
 
-git clone git@nnduc_github:nnduc/nnduc.github.io.git --branch publishing .deploy_git --depth 1  || exit 1
+git clone git@nnduc_github:nnduc/nnduc.github.io.git --branch publishing .deploy_git --depth 1
 
-rm -frd .deploy_git/* || exit 1
+rm -frd .deploy_git/*
 
-hexo generator || exit 1
-hexo deploy || exit 1
+hexo generator
+hexo deploy
